2d67f48239ffef79ca8bdbff76f4cb594d6c87a6
saify2j <bsse0924@iit.du.ac.bd>
2020-01-27 17:31:04 +0600

    lifecycle hooks added

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,28 @@
 <template>
   <div id="app">
      <Home/>
-     <AllFriends :friends="friends" @delete="deleteFriend"/>
-     <OnlineFriends :friends="friends"/>
+     <Blogs/>
 
   </div>
 </template>
 
 <script>
 import Home from './components/Home'
-import AllFriends from './components/AllFriends'
-import OnlineFriends from './components/OnlineFriends'
+import Blogs from './components/Blogs'
 export default {
   name: 'App',
   components: {
     Home,
-    AllFriends,
-    OnlineFriends
+    Blogs
   },
   data(){
     return{
-        friends: [
-            { name:'saif', online: true},
-            { name:'asif', online: true},
-            { name:'rafi', online: false},
-            { name:'rizvi', online: true},
-            { name:'drew', online: false},
-            
-        ]
+        
     }
   },
   methods:{
-    deleteFriend(payload){
-      this.friends = this.friends.filter(friend =>{
-        return friend.name !==payload.name
-      }
-
-      )
-      console.log(payload)
-    }
+    
   }
 }
 </script>
+/* eslint-disable */ 
---
+++ src/components/Blogs.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+        <button @click="changeTitle"> Change Title</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'Blogs',
+    data(){
+        return {
+            title: 'Dragonball Z'
+        }
+    },
+    methods:{
+        changeTitle(){
+            this.title = 'Naruto'
+        }
+    },
+    beforeCreate(){
+        alert('hello before created')
+    },
+    created(){
+        alert('hello after created')
+    },
+    beforeUpdate(){
+        alert('before update')
+    }
+}
+</script>
+/* eslint-disable */ 
